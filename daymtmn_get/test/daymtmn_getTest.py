e11b05a16e6637d318c35b45c709f1b9c98d087c
ayakangel <ayaka.tobe@gmail.com>
2024-11-30 18:14:18 +0900

    Add files via upload

---
+++ daymtmn_get/test/daymtmn_getTest.py
@@ -0,0 +1,299 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- Python -*-
+
+# <rtc-template block="description">
+"""
+ @file daymtmn_getTest.py
+ @brief ModuleDescription
+ @date $Date$
+
+
+"""
+# </rtc-template>
+
+from __future__ import print_function
+import sys
+import time
+sys.path.append(".")
+
+# Import RTM module
+import RTC
+import OpenRTM_aist
+
+
+# Import Service implementation class
+# <rtc-template block="service_impl">
+
+import daymtmn_get
+
+# </rtc-template>
+
+# Import Service stub modules
+# <rtc-template block="consumer_import">
+# </rtc-template>
+
+
+# This module's spesification
+# <rtc-template block="module_spec">
+daymtmn_gettest_spec = ["implementation_id", "daymtmn_getTest", 
+         "type_name",         "daymtmn_getTest", 
+         "description",       "ModuleDescription", 
+         "version",           "1.0.0", 
+         "vendor",            "Ayaka", 
+         "category",          "Category", 
+         "activity_type",     "STATIC", 
+         "max_instance",      "1", 
+         "language",          "Python", 
+         "lang_type",         "SCRIPT",
+         ""]
+# </rtc-template>
+
+# <rtc-template block="component_description">
+##
+# @class daymtmn_getTest
+# @brief ModuleDescription
+# 
+# 
+# </rtc-template>
+class daymtmn_getTest(OpenRTM_aist.DataFlowComponentBase):
+    
+    ##
+    # @brief constructor
+    # @param manager Maneger Object
+    # 
+    def __init__(self, manager):
+        OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
+
+        self._d_daymtmn_out = OpenRTM_aist.instantiateDataType(RTC.TimedString)
+        """
+        """
+        self._daymtmn_outIn = OpenRTM_aist.InPort("daymtmn_out", self._d_daymtmn_out)
+        self._d_day_in = OpenRTM_aist.instantiateDataType(RTC.TimedString)
+        """
+        """
+        self._day_inOut = OpenRTM_aist.OutPort("day_in", self._d_day_in)
+        self._d_mtmn_in = OpenRTM_aist.instantiateDataType(RTC.TimedString)
+        """
+        """
+        self._mtmn_inOut = OpenRTM_aist.OutPort("mtmn_in", self._d_mtmn_in)
+
+
+        
+
+
+        # initialize of configuration-data.
+        # <rtc-template block="init_conf_param">
+        
+        # </rtc-template>
+
+
+         
+    ##
+    #
+    # The initialize action (on CREATED->ALIVE transition)
+    # 
+    # @return RTC::ReturnCode_t
+    # 
+    #
+    def onInitialize(self):
+        # Bind variables and configuration variable
+        
+        # Set InPort buffers
+        self.addInPort("daymtmn_out",self._daymtmn_outIn)
+        
+        # Set OutPort buffers
+        self.addOutPort("day_in",self._day_inOut)
+        self.addOutPort("mtmn_in",self._mtmn_inOut)
+        
+        # Set service provider to Ports
+        
+        # Set service consumers to Ports
+        
+        # Set CORBA Service Ports
+        
+        return RTC.RTC_OK
+    
+    ###
+    ## 
+    ## The finalize action (on ALIVE->END transition)
+    ## 
+    ## @return RTC::ReturnCode_t
+    #
+    ## 
+    #def onFinalize(self):
+    #
+    #    return RTC.RTC_OK
+    
+    #    ##
+    ##
+    ## The startup action when ExecutionContext startup
+    ## 
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onStartup(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The shutdown action when ExecutionContext stop
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onShutdown(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The activated action (Active state entry action)
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ## 
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onActivated(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    #    ##
+    ##
+    ## The deactivated action (Active state exit action)
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onDeactivated(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ##
+    #
+    # The execution action that is invoked periodically
+    #
+    # @param ec_id target ExecutionContext Id
+    #
+    # @return RTC::ReturnCode_t
+    #
+    #
+    def onExecute(self, ec_id):
+    
+        return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The aborting action when main logic error occurred.
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    #    #
+    ##
+    #def onAborting(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The error action in ERROR state
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onError(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The reset action that is invoked resetting
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onReset(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The state update action that is invoked after onExecute() action
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+
+    ##
+    #def onStateUpdate(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The action that is invoked when execution context's rate is changed
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onRateChanged(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    def runTest(self):
+        return True
+
+def RunTest():
+    manager = OpenRTM_aist.Manager.instance()
+    comp = manager.getComponent("daymtmn_getTest0")
+    if comp is None:
+        print('Component get failed.', file=sys.stderr)
+        return False
+    return comp.runTest()
+
+def daymtmn_getTestInit(manager):
+    profile = OpenRTM_aist.Properties(defaults_str=daymtmn_gettest_spec)
+    manager.registerFactory(profile,
+                            daymtmn_getTest,
+                            OpenRTM_aist.Delete)
+
+def MyModuleInit(manager):
+    daymtmn_getTestInit(manager)
+    daymtmn_get.daymtmn_getInit(manager)
+
+    # Create a component
+    comp = manager.createComponent("daymtmn_getTest")
+
+def main():
+    mgr = OpenRTM_aist.Manager.init(sys.argv)
+    mgr.setModuleInitProc(MyModuleInit)
+    mgr.activateManager()
+    mgr.runManager(True)
+
+    ret = RunTest()
+    mgr.shutdown()
+
+    if ret:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
+
